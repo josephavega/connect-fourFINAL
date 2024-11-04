2d1f2cb9f16e2bddfbdca54ff85075f2be1fc60c
Lgzr <lsimons@oswego.edu>
2024-11-04 10:33:13 -0500

    Fixed Queue component

--- client/src/components/Queue.jsx
+++ client/src/components/Queue.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { io } from 'socket.io-client';
+import queueSocket from '../sockets/queueSocket';
 import '../styles/queue.css';
 
 const Queue = () => {

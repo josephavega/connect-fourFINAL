a62ada66ff16f7e41505d5aec408bad9ad1ac8c2
Logan <lsimons@oswego.edu>
2024-11-09 19:24:05 -0500

    added JoingamePopup component and updated sockets

--- client/src/pages/game.jsx
+++ client/src/pages/game.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import GameBoard from '../components/newGameboard';
+import GameBoard from '../components/Gameboard';
 import '../styles/game.css';
 import gameSocket from '../sockets/gameSocket';
 
